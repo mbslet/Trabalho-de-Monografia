f2fa34d14845d6b76474181124863f46fded7204
leticia.meneses <leticia.meneses@optelgroup.com>
2022-02-17 00:05:56 -0300

    set text on lp

--- proposal/src/components/LandingPage.vue
+++ proposal/src/components/LandingPage.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
     <Topbar/>
+    <div class="display-flex text">
+        <h1 class="title">Modelo de Reutilização
+        de Interface de Usuário:</h1>
+        <h2 class="subtitle">Um guia de aplicações</h2>
+    </div>
 </div>
 </template>
 
@@ -16,3 +21,30 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+.title{
+    padding: 80px;
+    padding-left: 1px;
+    margin-left: 10vh;
+    width: 130vh;
+    margin-top: 5vh;
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 80px;
+    color: #F3EDFC;
+}
+.subtitle{
+    font-family: 'Roboto', sans-serif;
+    color: #292929;
+    font-size: 40px;
+    margin-left: 10vh;
+    margin-top: -15vh;
+
+}
+.text{
+    margin-top: 10vh;
+}
+</style>
